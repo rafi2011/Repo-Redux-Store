43b83e3b2da4da2b6564bd410b0755f0a5e5447e
rafi2011 <rnessa2011@gmail.com>
2021-09-03 11:28:48 -0400

    updates app.js file

--- client/src/app.js
+++ client/src/app.js
@@ -8,14 +8,15 @@ import {
 } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
 
+import { Provider } from 'react-redux';
+import store from './utils/store';
+
 import Home from './pages/Home';
 import Detail from './pages/Detail';
 import NoMatch from './pages/NoMatch';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
 import Nav from './components/Nav';
-import { Provider } from 'react-redux';
-import store from './utils/store';
 import Success from './pages/Success';
 import OrderHistory from './pages/OrderHistory';
 
